38dc68f55b95667ef9d86e6df56622bd48197fac
FredKy <90769096+FredKy@users.noreply.github.com>
2022-11-08 15:57:45 +0100

    Initial Commit

---
+++ PokemonDB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokemonDB
+//
+//  Created by Fredrik Kypta on 2022-11-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PokemonDB/PokemonDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonDBApp.swift
+//  PokemonDB
+//
+//  Created by Fredrik Kypta on 2022-11-08.
+//
+
+import SwiftUI
+
+@main
+struct PokemonDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
